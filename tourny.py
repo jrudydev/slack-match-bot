92550691868a586308c375fc4c4b9002d5ef6385
jrudygomez <rudy@vessel.io>
2016-08-31 00:14:34 -0700

    tree integrated end-to-end

--- tourny.py
+++ tourny.py
@@ -2,96 +2,36 @@
 
 from player import Player
 from match import Match
+from tree import TournyTree
 from match import MATCH_STATUS_COMPLETE
 import random
 
 class Tourny:
   def __init__(self):
     self.__players = {}
-    self.__games = []
-    self.__unassigned_players_top = []
-    self.__unassigned_players_bottom = []
-    self.__bracket_size = None
-
-  def __pop_random_unassigned_player_top(self):
-    number_of_players = len(self.__unassigned_players_top)
-    if number_of_players == 0:
-      return None
-
-    rand_int = random.choice(range(number_of_players))
-    random_player = self.__unassigned_players_top[rand_int]
-    del self.__unassigned_players_top[rand_int]
-    return random_player
-
-  def __pop_random_unassigned_player_bottom(self):
-    number_of_players = len(self.__unassigned_players_bottom)
-    if number_of_players == 0:
-      return None
-
-    rand_int = random.choice(range(number_of_players))
-    random_player = self.__unassigned_players_bottom[rand_int]
-    del self.__unassigned_players_bottom[rand_int]
-    return random_player
+    self.__bracket = TournyTree()
   
-  def add_player(self, player):
+  def add(self, player):
     self.__players[player.get_user()] = player
+
+  def help(self, key):
+    # call the readmedocs app
+    pass
        
-  def start_tourny(self):
+  def start(self):
     number_of_players = len(self.__players)
     if number_of_players < 2:
       print "There are not enough players."
       return
     
-    number_of_games = number_of_players / 2
-    if number_of_players % 2 == 1:
-      number_of_games += 1
-
-    self.__bracket_size = 1
-    while self.__bracket_size < number_of_games:
-      self.__bracket_size *= 2
-
-    half_of_the_bracket = self.__bracket_size / 2
-
-    self.__games = []
-  
-    i = 0
+    players = []
     for key in self.__players:
-      if i < len(self.__players) - number_of_players / 2:
-        self.__unassigned_players_top.append(self.__players[key])
-      else:
-        self.__unassigned_players_bottom.append(self.__players[key])
-      i += 1
+      players.append(self.__players[key])
+    self.__bracket.generate(players)
 
-    for x in range(half_of_the_bracket):
-      first_player = self.__pop_random_unassigned_player_top()
-      if first_player != None:
-        first_player.set_match_id(x)
-      second_player = self.__pop_random_unassigned_player_top()
-      if second_player != None:
-        second_player.set_match_id(x)
-
-      match = Match()
-      match.add_side(first_player)
-      match.add_side(second_player)
-      self.__games.append(match)
-
-    for x in range(half_of_the_bracket):
-      first_player = self.__pop_random_unassigned_player_bottom()
-      if first_player != None:
-        first_player.set_match_id(x + half_of_the_bracket)
-      second_player = self.__pop_random_unassigned_player_bottom()
-      if second_player != None:
-        second_player.set_match_id(x + half_of_the_bracket)
-
-      match = Match()
-      match.add_side(first_player)
-      match.add_side(second_player)
-      self.__games.append(match)
-
-    self.print_tourny()
-
-  def report_win(self, user):
-    if len(self.__games) == 0:
+  def win(self, user):
+    games = self.__bracket.get_games()
+    if len(games) == 0:
       return "The tournament has not started."
 
     if user not in self.__players:
@@ -99,7 +39,7 @@ class Tourny:
     
     player = self.__players[user]
     game_id = player.get_match_id()
-    match = self.__games[game_id]
+    match = games[game_id]
     match.add_win(user)
 
     response = ""
@@ -108,26 +48,29 @@ class Tourny:
     else:
       response = player.get_name() + " repoted a win"
 
-    if len(self.__games) == 1 and self.is_complete():
+    if len(games) == 1 and self.is_complete():
       response += " and is the champion!"
     else:
       response += "."
 
     return response
 
-  def next_rount(sef):
+  def next(sef):
     if self.is_complete():
       print "Advance to the next round"
 
-  def get_printed_tourny(self):
+
+
+
+  def get_printed(self):
+    games = self.__bracket.get_round(1)
+
     string = ""
-    if len(self.__games) == 0:
-      output = "The tournament has not started."
-      print output
-      return output
+    if len(games) == 0:
+      return "The tournament has not started."
 
     i = 1
-    for match in self.__games:
+    for match in games:
       string = "%s\nMatch: %d\n" % (string, i)
       string = "%s%s\n" % (string, match.get_score())
       i += 1
@@ -135,19 +78,11 @@ class Tourny:
     return string
 
   def print_tourny(self):
-    print self.get_printed_tourny()
-
-  def show_players(self):
-    for key in self.__players:
-      player = self.__players[key]
-      print player.get_name()
-    return
-
-  def get_size(self):
-    return len(self.__players)
+    print self.get_printed()
 
   def is_complete(self):
-    for match in self.__games:
+    games = self.__bracket.get_games()
+    for match in games:
       if match.is_complete() == False:
         return False
     
@@ -156,16 +91,16 @@ class Tourny:
 
 def main():
   tourny = Tourny()
-  tourny.add_player(Player("U123", "abc", "fabc", "labc"))
-  tourny.add_player(Player("U456", "def", "fdef", "ldef"))
+  tourny.add(Player("U123", "abc", "fabc", "labc"))
+  tourny.add(Player("U456", "def", "fdef", "ldef"))
 
-  tourny.start_tourny()
+  tourny.start()
   print ""
   
   tourny.print_tourny()
   print ""
 
-  tourny.report_win("U123")
+  tourny.win("U123")
   print ""
 
   tourny.print_tourny()
